583692f610cefe89a21d0b4f22f05885aa71ec02
Achraf <117481103+8ird8@users.noreply.github.com>
2024-03-01 15:50:05 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -52,9 +52,9 @@ const AppRoutes: React.FC = () => {
       <Route
         path="/home"
         element={
-          <CheckToken>
+{/*           <CheckToken> */}
             <Home />
-          </CheckToken>
+          // </CheckToken>
         }
       />
       <Route
