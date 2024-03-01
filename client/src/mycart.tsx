c8aefac882e66a56efee028dd3b26d8647be1394
Achraf <117481103+8ird8@users.noreply.github.com>
2024-03-01 16:25:28 +0100

    Update mycart.tsx

--- client/src/mycart.tsx
+++ client/src/mycart.tsx
@@ -113,7 +113,7 @@ const MyCart = () => {
                   <div className="flex flex-row items-center">
                     <Avatar
                       alt={currentUserInfo.username}
-                      src={`http://localhost:3000/public/${currentUserInfo.avatar}`}
+                      src={`https://shopez-q69l.onrender.com/public/${currentUserInfo.avatar}`}
                       sx={{
                         width: 40,
                         height: 40,
@@ -156,7 +156,7 @@ const MyCart = () => {
                     <Card
                       Name={product.product_Name}
                       imageUrl={[
-                        `http://localhost:3000/public/${product.product_Images[0]}`,
+                        `https://shopez-q69l.onrender.com/public/${product.product_Images[0]}`,
                       ]}
                       price={product.product_Price}
                       productId={product._id}
